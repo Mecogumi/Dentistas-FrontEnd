035630496b9316863b60dae461fb74077df95c31
Ileana-Castillo <ileanag.castillo@gmail.com>
2025-11-06 16:23:45 -0600

    numero de telefono

--- src/app/layout/signup/signup.component.ts
+++ src/app/layout/signup/signup.component.ts
@@ -15,6 +15,7 @@ export class SignupComponent {
   signupForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required, Validators.minLength(10)]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
   });
 
