56e93aed16246329487598a533cff678110dadce
Ileana-Castillo <ileanag.castillo@gmail.com>
2025-11-06 16:29:33 -0600

    cambios xd

--- src/app/layout/appointment/appointment.ts
+++ src/app/layout/appointment/appointment.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Footer } from "../../shared/components/footer/footer";
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { DateColumn } from '../../shared/components/appointments/date-column/date-column';
 
 @Component({
   selector: 'app-appointment',
-  imports: [NavbarComponent, Footer],
+  imports: [NavbarComponent, Footer, DateColumn],
   templateUrl: './appointment.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
---
+++ src/app/shared/components/appointments/date-column/date-column.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-column',
+  imports: [],
+  templateUrl: './date-column.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DateColumn { }
